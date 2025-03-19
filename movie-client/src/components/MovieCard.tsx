025380a9cf0e7589c0c9a2f32404515052acc0b1
chinatip <chinatip.kultanapanit@gmail.com>
2025-03-19 15:08:23 +1100

    Update MovieCard style

--- movie-client/src/components/MovieCard.tsx
+++ movie-client/src/components/MovieCard.tsx
@@ -75,11 +75,7 @@ export const MovieCard: React.FC<MovieCardProps> = ({ movie }) => {
     if (!movieDetail) return;
 
     return (
-      <Accordion
-        type="single"
-        collapsible
-        className="min-w-2xs sm:min-w-md sm:max-w-md lg:min-w-lg lg:max-w-lg"
-      >
+      <Accordion type="single" collapsible>
         <AccordionItem value="item-1">
           <AccordionTrigger>Movie Details</AccordionTrigger>
           <AccordionContent>
@@ -100,7 +96,7 @@ export const MovieCard: React.FC<MovieCardProps> = ({ movie }) => {
   );
 
   const renderHeader = () => (
-    <CardHeader className="min-w-2xs sm:min-w-md sm:max-w-md lg:min-w-lg lg:max-w-lg">
+    <CardHeader>
       <div className="flex flex-col sm:flex-row">
         <div className="size-full sm:size-1 min-w-[100px] min-h-[153px] mb-4 sm:mr-4 sm:mb-0 flex justify-center">
           <MoviePoster url={poster} altText={title} />
@@ -116,7 +112,7 @@ export const MovieCard: React.FC<MovieCardProps> = ({ movie }) => {
   );
 
   return (
-    <Card className="text-left mb-4">
+    <Card className="w-full min-w-2xs sm:min-w-md sm:max-w-md lg:min-w-lg lg:max-w-lg mb-4 text-left">
       {renderHeader()}
       {renderContent()}
     </Card>
